677a2d92a07d1fc7e8dcaee7d495566e7a861c91
Swagat47 <swagatraj47@gmail.com>
2023-05-15 01:19:08 +0530

    cors edit

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,12 @@ const app = express();
 //console.log("-------------------------------------------------------");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use(morgan("dev"));
 app.use(cookieParser());
